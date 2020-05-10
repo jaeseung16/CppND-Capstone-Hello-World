0f625f679d9a9bb9910779a92c2ca09cc551b516
Jae-Seung Lee <jaeseung@gmail.com>
2020-05-10 10:12:24 -0400

    Remove ppm creation

--- src/main.cpp
+++ src/main.cpp
@@ -25,30 +25,19 @@ int value(int x, int y) {
 int main() {
     std::cout << "Hello World!" << "\n";
 
-    std::ofstream my_Image("mandelbrot.ppm");
-
     cv::Mat imageMat(width, height, CV_8UC3, cv::Vec3b(0,0,0));
-
-    if (my_Image.is_open()) {
-        my_Image << "P3\n" << width << " " << height << " 255\n";
-
-        auto start = std::chrono::high_resolution_clock::now();
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                int val = value(x, y);
-                my_Image << val << ' ' << 0 << ' ' << 0 << "\n";
-                imageMat.at<cv::Vec3b>(x, y) = cv::Vec3b(0,val,0);
-            }
+    
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int x = 0; x < width; x++) {
+        for (int y = 0; y < height; y++) {
+            int val = value(x, y);
+            imageMat.at<cv::Vec3b>(x, y) = cv::Vec3b(0,val,0);
         }
-        auto end = std::chrono::high_resolution_clock::now();
-        std::cout << "It took " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << std::endl;
-        my_Image.close();
-    } else {
-        std::cout << "Could not open the file\n";
     }
-
+    auto end = std::chrono::high_resolution_clock::now();
+    std::cout << "It took " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << std::endl;
+    
     cv::imshow("My image", imageMat);
-
     cv::waitKey(0);
 
     return 0;
