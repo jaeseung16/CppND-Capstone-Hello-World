3dcf9ff36e7b23e2091cfb148370f34fbcc3318d
Jae-Seung Lee <jaeseung@gmail.com>
2020-05-19 20:51:22 -0400

    Update MandelbrotSet
    Remove MandelbrotPoint

--- src/MandelbrotSet.cpp
+++ src/MandelbrotSet.cpp
@@ -17,31 +17,12 @@ MandelbrotSet::MandelbrotSet() {
 MandelbrotSet::MandelbrotSet(std::vector<std::complex<float>> &&zs, int nIteration) {
     _zs = zs;
     _nIteration = nIteration;
-/*
-    for (auto z : zs) {
-        MandelbrotPoint point = MandelbrotPoint(std::move(z), (unsigned int) _nIteration);
-        _iterations.push_back(point.getIteration());
-        _isMandelbrotSet.push_back(point.isMandelBrotSet());
-        _set.push_back(std::move(point));
-    }*/
-
-    std::cout << "### MandelbrotSet ###" << "\n";
-
     _values = std::vector<int>(_zs.size(), 0);;
 
-    //cv::Mat mandelbrotImg(800, 800, CV_8U);
     auto start = std::chrono::high_resolution_clock::now();
     parallel_for_(cv::Range(0, 800*800), [&](const cv::Range& range){
         for (int r = range.start; r < range.end; r++)
         {
-            /*
-            int i = r / 800;
-            int j = r % 800;
-
-            float x0 = j * 3.0 / 800.0 - 2.0;
-            float y0 = i * 3.0 / 800.0 - 1.5;
-
-            std::complex<float> z0(x0, y0);*/
             uchar value = (uchar) mandelbrotFormula(this->_zs[r], this->_nIteration);
             _values[r] = value;
         }
@@ -49,7 +30,6 @@ MandelbrotSet::MandelbrotSet(std::vector<std::complex<float>> &&zs, int nIterati
 
     auto end = std::chrono::high_resolution_clock::now();
     std::cout << "MandelbrotSet::MandelbrotSet took " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << std::endl;
-    std::cout << "_values[0] = " << _values[0] << std::endl;
 }
 
 int MandelbrotSet::mandelbrot(const std::complex<float> &z0, const int max)
@@ -60,7 +40,6 @@ int MandelbrotSet::mandelbrot(const std::complex<float> &z0, const int max)
         if (z.real()*z.real() + z.imag()*z.imag() > 4.0f) return t;
         z = z*z + z0;
     }
-
     return max;
 }
 
@@ -70,6 +49,5 @@ int MandelbrotSet::mandelbrotFormula(const std::complex<float> &z0, const int ma
     {
         return 0;
     }
-
     return cvRound(sqrt(value / (float) maxIter) * 255);
 }
--- src/MandelbrotSet.h
+++ src/MandelbrotSet.h
@@ -1,7 +1,6 @@
 #include <complex>
 #include <vector>
 #include <thread>
-#include "MandelbrotPoint.h"
 
 class MandelbrotSet
 {
@@ -9,19 +8,13 @@ public:
     MandelbrotSet();
     MandelbrotSet(std::vector<std::complex<float>> &&zs, int nIteration);
 
-    std::vector<MandelbrotPoint> getSet() { return _set; }
-    std::vector<bool> getIsMandelbrotSet() { return _isMandelbrotSet; }
-    std::vector<unsigned int> getIterations() { return _iterations; }
     std::vector<int> getValues() { return _values; }
 
 private:
     int mandelbrot(const std::complex<float> &z0, const int max);
     int mandelbrotFormula(const std::complex<float> &z0, const int maxIter);
 
-    std::vector<MandelbrotPoint> _set;
     std::vector<std::complex<float>> _zs;
-    std::vector<bool> _isMandelbrotSet;
-    std::vector<unsigned int> _iterations;
     std::vector<int> _values;
     int _nIteration = 50;
 };
