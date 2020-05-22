0c7b9faacfad0fc72733afec3d009dd6269e9066
Jae-Seung Lee <jaeseung@gmail.com>
2020-05-21 20:50:26 -0400

    Update MandelbrotColor
    Add convertToMandelbrotColor()

--- src/MandelbrotColor.cpp
+++ src/MandelbrotColor.cpp
@@ -1,6 +1,6 @@
 #include "MandelbrotColor.h"
 
-cv::Vec3b MandelbrotColor::convertToVec3b(MandelbrotColor::Color color) {
+cv::Vec3b MandelbrotColor::convertToVec3b(const MandelbrotColor::Color color) {
     cv::Vec3b colorToVec3b = cv::Vec3b(0,0,0);
     switch(color) {
         case MandelbrotColor::Color::Red:
@@ -23,4 +23,18 @@ cv::Vec3b MandelbrotColor::convertToVec3b(MandelbrotColor::Color color) {
             break;
     }
     return colorToVec3b;
+}
+
+MandelbrotColor::Color MandelbrotColor::convertToMandelbrotColor(const cv::Vec3b vec3b) {
+    MandelbrotColor::Color color = MandelbrotColor::Color::White;
+    if (vec3b == cv::Vec3b(255,255,0)) {
+        color = MandelbrotColor::Color::Cyan;
+    } else if (vec3b == cv::Vec3b(0,255,255)) {
+        color = MandelbrotColor::Color::Yellow;
+    } else if (vec3b == cv::Vec3b(0,0,255)) {
+        color = MandelbrotColor::Color::Red;
+    } else if (vec3b == cv::Vec3b(0,255,0)) {
+        color = MandelbrotColor::Color::Green;
+    }
+    return color;
 }
--- src/MandelbrotColor.h
+++ src/MandelbrotColor.h
@@ -5,6 +5,6 @@ class MandelbrotColor
     public:
     enum Color { Red, Yellow, Green, Cyan, Blue, White };
 
-    static cv::Vec3b convertToVec3b(MandelbrotColor::Color color);
+    static cv::Vec3b convertToVec3b(const MandelbrotColor::Color color);
+    static MandelbrotColor::Color convertToMandelbrotColor(const cv::Vec3b vec3b);
 };
-
