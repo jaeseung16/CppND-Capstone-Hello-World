b0887ed0419f87cb03c369d989666a0cf9c72f00
Jae-Seung Lee <jaeseung@gmail.com>
2020-05-23 20:21:37 -0400

    Update MandelbrotSet
    Clean up

--- src/MandelbrotSet.cpp
+++ src/MandelbrotSet.cpp
@@ -1,9 +1,8 @@
 #include <complex>
 #include <vector>
-#include <memory>
 #include <algorithm>
-#include <thread>
 #include <iostream>
+#include <math.h>
 
 #include <opencv2/core/utility.hpp>
 #include <opencv2/core/types.hpp>
@@ -12,114 +11,126 @@
 
 MandelbrotSet::MandelbrotSet() {}
 
-MandelbrotSet::~MandelbrotSet() {}
+MandelbrotSet::~MandelbrotSet()
+{
+    _zs.clear();
+    _values.clear();
+}
 
-MandelbrotSet::MandelbrotSet(const MandelbrotSet &source) {
-    for (auto it = std::begin(source._zs); it != std::end(source._zs); ++it) {
+MandelbrotSet::MandelbrotSet(const MandelbrotSet &source)
+{
+    for (auto it = std::begin(source._zs); it != std::end(source._zs); ++it)
+    {
         _zs.push_back(*it);
     }
 
-    for (auto it = std::begin(source._values); it != std::end(source._values); ++it) {
+    for (auto it = std::begin(source._values); it != std::end(source._values); ++it)
+    {
         _values.push_back(*it);
     }
 
-    _nIteration = source._nIteration;
+    _maxIter = source._maxIter;
 }
 
-MandelbrotSet &MandelbrotSet::operator=(const MandelbrotSet &source) {
+MandelbrotSet &MandelbrotSet::operator=(const MandelbrotSet &source)
+{
     if (this == &source)
          return *this;
 
-    for (auto it = std::begin(source._zs); it != std::end(source._zs); ++it) {
+    for (auto it = std::begin(source._zs); it != std::end(source._zs); ++it)
+    {
         _zs.push_back(*it);
     }
 
-    for (auto it = std::begin(source._values); it != std::end(source._values); ++it) {
+    for (auto it = std::begin(source._values); it != std::end(source._values); ++it)
+    {
         _values.push_back(*it);
     }
 
-    _nIteration = source._nIteration;
+    _maxIter = source._maxIter;
 
     return *this;
 }
 
-MandelbrotSet::MandelbrotSet(MandelbrotSet &&source) {
+MandelbrotSet::MandelbrotSet(MandelbrotSet &&source)
+{
     _zs.clear();
     _values.clear();
 
-    for (auto it = std::begin(source._zs); it != std::end(source._zs); ++it) {
+    for (auto it = std::begin(source._zs); it != std::end(source._zs); ++it)
+    {
         _zs.push_back(std::move(*it));
     }
 
-    for (auto it = std::begin(source._values); it != std::end(source._values); ++it) {
+    for (auto it = std::begin(source._values); it != std::end(source._values); ++it)
+    {
         _values.push_back(std::move(*it));
     }
 
-    _nIteration = source._nIteration;
+    _maxIter = source._maxIter;
     
     source._zs.clear();
     source._values.clear();
-    source._nIteration = 0;
+    source._maxIter = 0;
 }
 
-MandelbrotSet &MandelbrotSet::operator=(MandelbrotSet &&source) {
+MandelbrotSet &MandelbrotSet::operator=(MandelbrotSet &&source)
+{
     if (this == &source)
          return *this;
 
     _zs.clear();
     _values.clear();
 
-    for (auto it = std::begin(source._zs); it != std::end(source._zs); ++it) {
+    for (auto it = std::begin(source._zs); it != std::end(source._zs); ++it)
+    {
         _zs.push_back(std::move(*it));
     }
 
-    for (auto it = std::begin(source._values); it != std::end(source._values); ++it) {
+    for (auto it = std::begin(source._values); it != std::end(source._values); ++it)
+    {
         _values.push_back(std::move(*it));
     }
 
-    _nIteration = source._nIteration;
+    _maxIter = source._maxIter;
     
     source._zs.clear();
     source._values.clear();
-    source._nIteration = 0;
+    source._maxIter = 0;
 
     return *this;
 }
 
-MandelbrotSet::MandelbrotSet(std::vector<std::complex<float>> &&zs, int nIteration, int size) {
-    _zs = zs;
-    _nIteration = nIteration;
+MandelbrotSet::MandelbrotSet(std::vector<std::complex<float>> &&zs, int maxIter)
+{
+    _zs = std::move(zs);
+    _maxIter = maxIter;
     _values = std::vector<int>(_zs.size(), 0);;
 
     auto start = std::chrono::high_resolution_clock::now();
-    parallel_for_(cv::Range(0, size*size), [&](const cv::Range& range){
-        for (int r = range.start; r < range.end; r++)
+    parallel_for_(cv::Range(0, _zs.size()), [&](const cv::Range& range)
         {
-            uchar value = (uchar) mandelbrotFormula(this->_zs[r], this->_nIteration);
-            _values[r] = value;
+            for (int r = range.start; r < range.end; r++)
+            {
+                auto value = mandelbrotFormula(this->_zs[r], this->_maxIter);
+                _values[r] = value;
+            }
         }
-    });
-
+    );
     auto end = std::chrono::high_resolution_clock::now();
-    std::cout << "MandelbrotSet::MandelbrotSet took " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << std::endl;
+    std::cout << "Generating Mandelbrot values took " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << std::endl;
 }
 
-int MandelbrotSet::mandelbrot(const std::complex<float> &z0, const int max)
+int MandelbrotSet::mandelbrotFormula(const std::complex<float> &z0, const int &maxIter)
 {
     std::complex<float> z = z0;
-    for (int t = 0; t < max; t++)
+    int iter = 0;
+    while (iter < maxIter)
     {
-        if (z.real()*z.real() + z.imag()*z.imag() > 4.0f) return t;
+        if (std::norm(z) > 4.0f) break;
         z = z*z + z0;
+        iter ++;
     }
-    return max;
-}
 
-int MandelbrotSet::mandelbrotFormula(const std::complex<float> &z0, const int maxIter=50) {
-    int value = mandelbrot(z0, maxIter);
-    if(maxIter - value == 0)
-    {
-        return 0;
-    }
-    return cvRound(sqrt(value / (float) maxIter) * 255);
+    return (maxIter == iter) ? 0 : (int)((float)iter / (float)maxIter * 255.0);
 }
--- src/MandelbrotSet.h
+++ src/MandelbrotSet.h
@@ -1,6 +1,5 @@
 #include <complex>
 #include <vector>
-#include <thread>
 
 class MandelbrotSet
 {
@@ -12,15 +11,14 @@ public:
     MandelbrotSet(MandelbrotSet &&source);
     MandelbrotSet &operator=(MandelbrotSet &&source);
 
-    MandelbrotSet(std::vector<std::complex<float>> &&zs, int nIteration, int size);
+    MandelbrotSet(std::vector<std::complex<float>> &&zs, int maxIter);
 
     std::vector<int> getValues() { return _values; }
 
 private:
-    int mandelbrot(const std::complex<float> &z0, const int max);
-    int mandelbrotFormula(const std::complex<float> &z0, const int maxIter);
+    int mandelbrotFormula(const std::complex<float> &z0, const int &maxIter);
 
     std::vector<std::complex<float>> _zs;
     std::vector<int> _values;
-    int _nIteration = 50;
+    int _maxIter = 50;
 };
